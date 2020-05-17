591d41c454017fc559233d71aecbe4e3ea84f07a
Ilya P <togowiththeflow@gmail.com>
2020-05-17 10:52:42 +0000

    Added hello world echo http server

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('Hello from deno');
+import { serve } from "https://deno.land/std@master/http/server.ts";
+
+const server = serve({
+	port: 3001,
+	hostname: '0.0.0.0',
+});
+
+console.log("The server running on: http://localhost:3001");
+
+for await (const request of server) {
+	request.respond({
+		body: "Hello World\n",
+	});
+}
